fb047016d8982e698d3d87254d475a34d2af6791
gichobih <dennismuri254@gmail.com>
2024-10-03 04:02:10 -0700

    0x01-ES6_promise

--- 0x01-ES6_promise/7-load_balancer.js
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -1,7 +1,5 @@
 export default function loadBalancer(chinaDownload, USDownload) {
   return Promise
     .race([chinaDownload, USDownload])
-    .then((res) => {
-      return res;
-    });
+    .then((res) => res);
 }
