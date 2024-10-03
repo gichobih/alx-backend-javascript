2cd43acf38698bd054ca5dce5439a15ea7762f68
gichobih <dennismuri254@gmail.com>
2024-10-03 04:36:33 -0700

    0x01-ES6_promise

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,13 +1,13 @@
-export default function guardrail(mathFunction){
- const queue = [];
+export default function guardrail(mathFunction) {
+  const queue = [];
 
- try {
-  queue.push(mathFunction());
- } catch (err){
-  queue.push(String(err));
- } finally {
-  queue.push('Guardrail was processed');
- }
+  try {
+    queue.push(mathFunction());
+  } catch (err) {
+    queue.push(String(err));
+  } finally {
+    queue.push('Guardrail was processed');
+  }
 
- return queue;
+  return queue;
 }
