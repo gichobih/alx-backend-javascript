845c709e4efd308b0946fc659b0b3aa7404dde68
gichobih <dennismuri254@gmail.com>
2024-10-02 23:52:50 -0700

    0x01-ES6_promise

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,10 @@
+import { uploadProfile, createUser } from './utils';
+
+export default function handleProfileSignup() {
+    return Promise
+        .all([uploadProfile(), createUser()])
+        .then((res) => {
+            console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
+        })
+        .catch(() => console.log('Signup system offline'));
+}
