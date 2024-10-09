be0da8a5db4f0498d76e6726b4d519c539589481
gichobih <dennisgichobih@gmail.com>
2024-10-09 13:07:59 +0300

    0-get_list_stundents

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,19 @@
-export default function getListStundets{
-    return[
-        { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-        { id: 2, firstName: 'James', location: 'Columbia' },
-        { id: 5, firstName: 'Serena', location: 'San Francisco' }
-    ]
-}
+export default function getListStudents() {
+    const students = {
+      id: 1,
+      firstName: 'Guillaume',
+      location: 'San Francisco',
+    },
+    {
+      id: 2,
+      firstName: 'James',
+      location: 'Columbia',
+    },
+    {
+      id: 5,
+      firstName: 'Serena',
+      location: 'San Francisco',
+    },
+  
+    return [students];
+  }
